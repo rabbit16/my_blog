e2cdf6e0c9048ce702cd4b51bdce742476f69081
rabbit16 <1214662198@qq.com>
2020-02-11 11:10:07 +0800

    master

---
+++ apps/user/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from user import views
+app_name = 'user'
+urlpatterns = [
+    path('', views.Index.as_view(), name ='enter'),
+    path('blog/', views.ContextShow.as_view(), name ='blog'),
+    # path('intro/', views.Intro.as_view(), name ='intro'),
+    # path('article/', views.ContextShow.as_view(), name ='article'),
+]
